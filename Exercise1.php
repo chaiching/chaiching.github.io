642c92d1f424b6468b7dd621bb436cba3d42d405
Chai Ching <cheachaiching@gmail.com>
2021-10-13 10:56:22 +0800

    add variable and add two type ways to state age

--- Exercise1.php
+++ Exercise1.php
@@ -3,10 +3,16 @@
 		<body>
 			<?php
 				echo "<b> Chea Chai Ching </b>";
+				/*This is the saperate line to make word become column*/
 				echo "<br>";
 				echo "202-059";
 				echo "<br>";
 				echo date("l jS \of F Y");
+				echo "<br>";
+				$age = 19;
+				echo "I am $age years old";
+				echo "<br>";
+				echo "I am " . $age . " years old";
 			?>
 		</body>
 	</html>
