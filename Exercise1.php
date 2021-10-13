c07a452a2be16cdba7fc4d8e5640dd8d2c2f3a36
Chai Ching <cheachaiching@gmail.com>
2021-10-13 10:32:57 +0800

    add <br> to saperate down and add date

--- Exercise1.php
+++ Exercise1.php
@@ -2,8 +2,11 @@
 	<html>
 		<body>
 			<?php
-				echo "Chea Chai Ching"; 
+				echo "<b> Chea Chai Ching </b>";
+				echo "<br>";
 				echo "202-059";
+				echo "<br>";
+				echo date("l jS \of F Y");
 			?>
 		</body>
 	</html>
